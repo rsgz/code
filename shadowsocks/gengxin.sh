0a5bb19396dc582c93d0519b5715474603800b11
rsgz <52986175+rsgz@users.noreply.github.com>
2020-03-16 15:21:44 +0800

    Update gengxin.sh

--- shadowsocks/gengxin.sh
+++ shadowsocks/gengxin.sh
@@ -1,6 +1,6 @@
 # 清除大部分端口
-firewall-cmd --zone=public --remove-port=${xinduan}/tcp --permanent  &&
-firewall-cmd --reload  &&
+# firewall-cmd --zone=public --remove-port=${xinduan}/tcp --permanent  &&
+# firewall-cmd --reload  &&
 
 # 修改端口
 file="/etc/ss-config.json"  
