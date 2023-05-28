ff9fe353b47d4ed03c7bd78ffc58f5f3f0a166af
rsgz <52986175+rsgz@users.noreply.github.com>
2023-05-28 12:19:05 +0800

    将Django改成Flask
    将Django改成Flask
    因为现在的练习针对的是Flask
    不是Django

---
+++ Flask_lianxi/Flask-v5-get 方式获取表单数据/web1.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/register", methods=["GET"])  # 这个限制了 访问网址只能使用get请求
+def register():
+    return render_template("register.html")
+
+@app.route("/do/reg", methods=["GET"])
+def do_register():
+    print(request.args)
+    return "注册成功!!!"
+
+if __name__ == '__main__':
+    app.run(host="", port="8888")  # http://127.0.0.1:8888
