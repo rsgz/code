21e2fec60f7abfa590c3e20b0bc0b5b01306f6a0
rsgz <52986175+rsgz@users.noreply.github.com>
2023-05-28 09:49:30 +0800

    返回用户列表
    v3可以网页上面显示用户列表

---
+++ django_lianxi/django-v3-返回用户列表/web1.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/show/info")
+def index():
+    return render_template("index.html")  # http://127.0.0.1:8888/show/info
+
+@app.route("/get/news")
+def get_news():
+    return render_template("get_news.html")  # http://127.0.0.1:8888/get/news
+
+@app.route("/get/table1")
+def get_table1():
+    return render_template("table.html")  # http://127.0.0.1:8888/get/table1
+
+@app.route("/user/list")
+def user_list():
+    return render_template("user_list.html")  # http://127.0.0.1:8888/get/table1
+
+
+if __name__ == '__main__':
+    app.run(host="", port="8888")  # http://127.0.0.1:8888
