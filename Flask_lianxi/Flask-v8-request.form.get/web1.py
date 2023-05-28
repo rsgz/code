9beae92b1367055a4f6329a45f0df4c711ac3aea
rsgz <52986175+rsgz@users.noreply.github.com>
2023-05-28 12:55:49 +0800

    request.form.get
    request.form.get

---
+++ Flask_lianxi/Flask-v8-request.form.get/web1.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/register", methods=["GET"])  # 这个限制了 访问网址只能使用get请求
+def register():
+    return render_template("register.html")
+
+@app.route("/do/reg", methods=["GET"])
+def do_register():
+    print(request.args)
+    return "注册成功!!!"
+
+@app.route("/post/reg", methods=["POST"])
+def do_post_register():
+    user=request.form.get("user")
+    pwd=request.form.get("pwd")
+    gender=request.form.get("gender")
+    hobby_list=request.form.getlist("hobby")
+    city=request.form.get("city")
+    skill_list=request.form.getlist("skill")
+    more = request.form.get("more")
+    return "注册成功!!!"+f"{user},{pwd},{gender},{hobby_list},{city},{skill_list},{more}"
+
+if __name__ == '__main__':
+    app.run(host="", port="8888")  # http://127.0.0.1:8888
